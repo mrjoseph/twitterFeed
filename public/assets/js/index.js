f681f34b1b25ee7d04e9c6662a7e798c23dd5091
mrjoseph <trev_jos@hotmail.com>
2013-01-11 18:21:44 +0000

    now loading tweets as you scroll

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,30 +1,65 @@
 (function($, window, undefined){
 	var twitterFeed = twitterFeed || {};
 	twitterFeed = {
-		searchTerm : 'jayz',
-		twitterURL : 'http://search.twitter.com/search.json?q=jayz&rpp=5&include_entities=true&result_type=mixed',
-		getTweet: function(){
+		tweetNum : 10,
+		initLoad : true,
+		getTweet: function(num){
+			$('#ajax-loader').fadeIn();
+			var
+			searchTerm = 'Lewis Hamilton',
+			twitterURL = 'http://search.twitter.com/search.json?q='+ searchTerm +'&rpp='+ twitterFeed.tweetNum +'&include_entities=true&result_type=mixed';
 			$.ajax({
-				url :this.twitterURL,
+				url :twitterURL,
 				type: 'get',
 				dataType: 'jsonp',
 				contentType: "application/json",
 				success : function(feed){
-					console.log(feed.results);
-					var tweets = feed.results.length, results = '', i;
+					// console.log(feed.results[twitterFeed.tweetNum + 1]);
+					var tweets = feed.results.length, results = '', i, id;
 					for(i=0;i<tweets;i++){
-						results += '<div>'+ feed.results[i].from_user +'</div>';
+						id = feed.results[i].from_user_id_str;
+
+						results += '<dl style="" class="newTweet" id="'+ feed.results[i].from_user_id_str +'">';
+						results += '<dt>'+ feed.results[i].from_user +'</dt>';
+						results += '<dd><img src="'+ feed.results[i].profile_image_url_https +'"to alt="'+  feed.results[i].from_user_name +'"/></dd>';
+						results += '<dd>'+ feed.results[i].text +'</dd>';
+						results += '</dl>';
 					}
 					$('#tweets-list').html(results);
+					$('#'+id).hide().fadeIn();
+					$('#ajax-loader').fadeOut(function(){
+					
+						// console.log(id);
+					});
+
 				}
 			});
 		},
 		init:function(){
-
+			var _this = this;
+			$('#loadTweets').on('click',function(e){
+				e.preventDefault();
+				console.log(twitterFeed.tweetNum);
+				_this.getTweet();
+				_this.tweetNum ++;
+			});
+			$(window).scroll(function(){
+				var
+				docHeight = $(document).height(),
+				winHeight = $(window).height() + 1;
+				// console.log('document height:' + docHeight);
+				// console.log('window height:' + winHeight);
+				if($(window).scrollTop() >= ( docHeight - winHeight)){
+					
+					_this.getTweet();
+					_this.tweetNum = _this.tweetNum + 1;
+				}
+			});
+			_this.getTweet();
 		}
 	};
 
 	$(function(){
-		twitterFeed.getTweet();
+		twitterFeed.init();
 	});
 }(jQuery,window));
